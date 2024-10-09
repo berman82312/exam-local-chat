71e1ed99d23bf88be9fc549764592bac0450f54a
Fish Chan <fish.chan@outland.tw>
2024-10-09 14:51:29 +0800

    feat: handle enter pressed in message sender

--- packages/app/components/ChatMessageSender.tsx
+++ packages/app/components/ChatMessageSender.tsx
@@ -1,12 +1,26 @@
 import { Send } from "@mui/icons-material";
 import { IconButton, TextField } from "@mui/material";
+import { useState } from "react";
 
 export const ChatMessageSender = () => {
+  const [message, setMessage] = useState("");
+
+  const onKeyDown: React.KeyboardEventHandler = function (e) {
+    if (e.key === "Enter" && e.shiftKey) {
+      e.preventDefault();
+      setMessage("");
+    }
+  };
+
   return (
     <div className="p-4 border-t border-gray-400">
       <TextField
         fullWidth
         placeholder="Send a message..."
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        multiline
+        onKeyDown={onKeyDown}
         slotProps={{
           input: {
             endAdornment: (
